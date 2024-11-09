ec0b3503d40025c45c85c4b2612fb50735e64272
ungheung <t01047377477@gmail.com>
2024-11-09 15:22:54 +0900

    feat : 태그 컴포넌트 추가

---
+++ src/components/tag/Tag.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import icons from "../../assets/icon.png";
+import style from "./styles/tag.module.css";
+
+const Tag = ({ keyword }: { keyword: string }) => {
+  return (
+    <article className={style.tagWrap}>
+      <span className={style.tagIcon}>
+        <Image src={icons} alt={"태그_아이콘"} width={100} height={100} />
+      </span>
+      <span className={style.tagKeyword}>{keyword}</span>
+    </article>
+  );
+};
+
+export default Tag;
