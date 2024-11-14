b454f0e9d1de9b288c04c365a0558d9135bc918e
ungheung <t01047377477@gmail.com>
2024-11-14 19:33:58 +0900

    feat : 토큰 관리용 함수 추가

---
+++ src/components/common/constants/accessToken.ts
@@ -0,0 +1,29 @@
+export const token = {
+  access: "",
+};
+
+export const setToken = ({
+  isAccess,
+  accessToken,
+  refreshToken,
+}: {
+  isAccess?: boolean;
+  accessToken?: string;
+  refreshToken?: string;
+}) => {
+  if (isAccess === undefined || isAccess) {
+    token.access = accessToken!;
+  }
+
+  if (isAccess === undefined || !isAccess) {
+    localStorage.setItem("refreshToken", refreshToken!);
+  }
+};
+
+export const getToken = (isAccess: boolean) => {
+  if (isAccess) {
+    return token.access;
+  }
+
+  return localStorage.getItem("refreshToken");
+};
