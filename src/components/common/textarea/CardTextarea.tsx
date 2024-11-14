0f34518ad5420fcaf548d7259cfcedf3c3262755
ungheung <t01047377477@gmail.com>
2024-11-14 19:37:24 +0900

    feat : 카드용 input, textarea 추가

---
+++ src/components/common/textarea/CardTextarea.tsx
@@ -0,0 +1,9 @@
+import BaseTextarea, { BaseTextareaProps } from "./BaseTextarea";
+
+export interface CardTextareaProps extends BaseTextareaProps {}
+
+const CardTextarea = ({ id, name, className, onChange, placeholder }: CardTextareaProps) => {
+  return <BaseTextarea id={id} name={name} className={className} onChange={onChange} placeholder={placeholder} />;
+};
+
+export default CardTextarea;
