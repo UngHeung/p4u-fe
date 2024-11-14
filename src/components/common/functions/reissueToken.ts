dcc9fc558dbec9e44e551446754341b903b4d9ac
ungheung <t01047377477@gmail.com>
2024-11-14 19:36:01 +0900

    feat : 토큰 재발급 추가

---
+++ src/components/common/functions/reissueToken.ts
@@ -0,0 +1,42 @@
+import { refreshAxios } from "@/apis/axiosInstance";
+import { setToken } from "../constants/accessToken";
+import { BASE_URL } from "../constants/baseUrl";
+
+const reissueToken = async (isAccess: boolean) => {
+  try {
+    if (isAccess) {
+      const accessToken = await reissueAccessToken();
+      setToken({ isAccess, accessToken });
+      return accessToken;
+    } else {
+      const refreshToken = await reissueRefreshToken();
+      setToken({ isAccess, refreshToken });
+      return refreshToken;
+    }
+  } catch (error: any) {
+    console.error;
+  }
+};
+
+export default reissueToken;
+
+const accessReissueUrl = `${BASE_URL}/auth/reissue/access`;
+const refreshReissueUrl = `${BASE_URL}/auth/reissue/refresh`;
+
+/**
+ * Reissue access token
+ */
+export const reissueAccessToken = async () => {
+  const response = await refreshAxios.post(accessReissueUrl);
+  const accessToken = response.data.accessToken;
+  return accessToken;
+};
+
+/**
+ * Reissue refresh token
+ */
+export const reissueRefreshToken = async () => {
+  const response = await refreshAxios.post(refreshReissueUrl);
+  const refreshToken = response.data.refreshToken;
+  return refreshToken;
+};
