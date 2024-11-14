21013be3ef18910ba8950e6e2628be60c46c8ee4
ungheung <t01047377477@gmail.com>
2024-11-14 19:38:40 +0900

    feat : 본인 카드 응답처리 기능 추가

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,21 +1,49 @@
 "use client";
 
+import { authAxios } from "@/apis/axiosInstance";
+import { UserProps, UserStore, useUserStore } from "@/stores/user/userStore";
 import { useState } from "react";
+import { BASE_URL } from "../common/constants/baseUrl";
 import { svgIcons } from "../common/functions/getSvg";
 import Tag from "../tag/Tag";
 import style from "./styles/card.module.css";
 
 export interface CardProps {
-  writer: any;
+  id: number;
+  writer: UserProps;
   title: string;
   content: string;
-  intercessors: any[];
+  pickers: UserProps[];
   tags: any[];
-  answered: boolean;
+  isAnswered: boolean;
+  isAnonymity: boolean;
 }
 
-const Card = ({ writer, title, content, intercessors, tags, answered }: CardProps) => {
+const Card = ({ id, writer, title, content, pickers, tags, isAnswered, isAnonymity }: CardProps) => {
+  const user = useUserStore((state: UserStore) => state.user);
+
   const [isFliped, setIsFliped] = useState(false);
+  const [answered, setIsAnswered] = useState<boolean>(isAnswered);
+  const [disabled, setDisabled] = useState(false);
+
+  const handleAnswered = async () => {
+    setIsAnswered((prev) => !prev);
+    setDisabled(true);
+
+    const data = {
+      isAnswered: !answered,
+    };
+
+    try {
+      const url = `${BASE_URL}/card/${id}/answered`;
+      const response = await authAxios.patch(url, data);
+
+      setIsAnswered(response.data.isAnswered);
+    } catch (error: any) {
+    } finally {
+      setDisabled(false);
+    }
+  };
 
   return (
     <article
@@ -27,21 +55,40 @@ const Card = ({ writer, title, content, intercessors, tags, answered }: CardProp
         <header className={style.cardHeader}>
           <span className={style.cardInfo}>
             {svgIcons.heart()}
-            <strong>{`${intercessors.length}명`}</strong>
+            <strong>{`${pickers ? pickers.length : 0}명`}</strong>
             <span>{"이 이 기도제목을 위해 기도하고있습니다."}</span>
           </span>
 
-          <span>{answered ? svgIcons.answered() : svgIcons.checked(false)}</span>
+          <span>
+            {user.id === writer.id ? (
+              <button
+                type="button"
+                name="isAnswered"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleAnswered();
+                }}
+                disabled={disabled}
+              >
+                {svgIcons.answered()}
+              </button>
+            ) : answered ? (
+              svgIcons.answered()
+            ) : (
+              svgIcons.checked(false)
+            )}
+          </span>
         </header>
         <strong className={style.cardTitle}>{title}</strong>
-        <span className={style.cardWriter}>{writer.name}</span>
+        <span className={style.cardWriter}>{isAnonymity ? "익명" : writer.name}</span>
         <section className={style.cardTagWrap}>
           <ul className={style.cardTagList}>
-            {tags.length > 0 &&
-              tags.map((item, idx) => {
+            {tags &&
+              tags.length > 0 &&
+              tags.map((tag, idx) => {
                 return (
                   <li key={idx}>
-                    <Tag keyword={item} answered={answered} />
+                    <Tag keyword={tag.keyword} answered={answered} />
                   </li>
                 );
               })}
@@ -53,10 +100,28 @@ const Card = ({ writer, title, content, intercessors, tags, answered }: CardProp
         <header className={style.cardHeader}>
           <span className={style.cardInfo}>
             {svgIcons.heart()}
-            <strong>{`${intercessors.length}명`}</strong>
+            <strong>{`${pickers ? pickers.length : 0}명`}</strong>
             <span>{"이 이 기도제목을 위해 기도하고있습니다."}</span>
           </span>
-          <span>{answered ? svgIcons.answered() : svgIcons.checked(false)}</span>
+          <span>
+            {user.id === writer.id ? (
+              <button
+                type="button"
+                name="isAnswered"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleAnswered();
+                }}
+                disabled={disabled}
+              >
+                {svgIcons.answered()}
+              </button>
+            ) : answered ? (
+              svgIcons.answered()
+            ) : (
+              svgIcons.checked(false)
+            )}
+          </span>
         </header>
 
         <pre className={style.cardContent}>{content}</pre>
