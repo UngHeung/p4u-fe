a49a281eea44ce7b274f250926e247ed848de5f6
ungheung <t01047377477@gmail.com>
2024-11-18 22:14:42 +0900

    update : next.config 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,50 @@
 import type { NextConfig } from 'next';
 
+const PROTOCOL = process.env.NEXT_PUBLIC_PROTOCOL;
+const AWS_DB_HOST = process.env.NEXT_PUBLIC_DB_HOST;
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
   async headers() {
-    return [];
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: `https://${AWS_DB_HOST}`,
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 
   async rewrites() {
-    return [];
+    return [
+      {
+        source: '/auth/:path*',
+        destination: `${PROTOCOL}://${AWS_DB_HOST}:3000/auth/:path*`,
+      },
+      {
+        source: '/card/:path*',
+        destination: `${PROTOCOL}://${AWS_DB_HOST}/user/:path*`,
+      },
+      {
+        source: '/user/:path*',
+        destination: `${PROTOCOL}://${AWS_DB_HOST}/team/:path*`,
+      },
+    ];
   },
 };
 
