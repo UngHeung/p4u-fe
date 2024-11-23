f34a1db7978a8d22d268d30718a9ed01e8fa7078
ungheung <t01047377477@gmail.com>
2024-11-23 20:56:11 +0900

    fix : 토큰 재발급 무한로딩 수정

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,85 +1,80 @@
 import { getToken } from '@/components/common/constants/accessToken';
 import reissueToken from '@/components/common/functions/reissueToken';
-import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
 
-export const baseAxios = axios.create({
+const createAxiosInstance = (config: AxiosRequestConfig = {}) => {
+  return axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    ...config,
+  });
+};
+
+export const baseAxios = createAxiosInstance();
+export const authAxios = createAxiosInstance();
+export const refreshAxios = createAxiosInstance({
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': undefined,
   },
 });
 
-export const authAxios = axios.create({
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-authAxios.interceptors.request.use(
-  config => callbackRequestConfig(config, true),
-  error => callbackRequestError(error),
-);
-
-authAxios.interceptors.response.use(
-  response => callbackResponse(response),
-  error => callbackResponseError(error, true),
-);
-
-export const refreshAxios = axios.create({});
-
-refreshAxios.interceptors.request.use(
-  config => callbackRequestConfig(config, false),
-  error => callbackRequestError(error),
-);
-
-export const callbackRequestConfig = (
+const setAuthAxios = (
   config: InternalAxiosRequestConfig,
   isAccess: boolean,
 ) => {
-  if (isAccess) {
-    const accessToken = getToken(true);
-    config.headers['Authorization'] = `Bearer ${accessToken}`;
-  } else {
-    const refreshToken = getToken(false);
-    config.headers['Authorization'] = `Bearer ${refreshToken}`;
-  }
-
+  const token = getToken(isAccess);
+  config.headers['Authorization'] = `Bearer ${token}`;
   return config;
 };
 
-export const callbackRequestError = (error: any) => {
-  return Promise.reject(error);
-};
+authAxios.interceptors.request.use(
+  config => setAuthAxios(config, true),
+  error => Promise.reject(error),
+);
 
-export const callbackResponse = (response: AxiosResponse) => {
-  if (response.status === 404) {
-    console.log('404 page');
-  }
-
-  return response;
-};
-
-export const callbackResponseError = async (error: any, isAccess: boolean) => {
-  if (error.response?.status === 401) {
-    try {
-      const refreshTokenResponse = await reissueToken(isAccess);
-
-      if (!refreshTokenResponse) {
-        return refreshTokenResponse;
-      }
-
-      const { config } = error;
-
-      config.header = {
-        ...config.header,
-        Authorization: `Bearer ${refreshTokenResponse}`,
-      };
-
-      const response = await authAxios.request(config);
-
-      return response;
-    } catch (error) {
-      return Promise.reject(error);
+authAxios.interceptors.response.use(
+  (response: AxiosResponse) => {
+    if (response.status === 404) {
+      console.log('404 page');
     }
-  }
-  return Promise.reject(error);
-};
+
+    return response;
+  },
+  async (error: any) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      originalRequest &&
+      !originalRequest?._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const newToken = await reissueToken(true);
+
+        if (!newToken) {
+          throw new Error('토큰 재발급 실패');
+        }
+
+        originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+        return authAxios.request(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+refreshAxios.interceptors.request.use(
+  config => setAuthAxios(config, false),
+  error => Promise.reject(error),
+);
