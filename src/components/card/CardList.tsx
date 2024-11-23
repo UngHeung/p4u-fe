63c8252e5ad4cd3cf8c79b34b6c02d511bf284e2
ungheung <t01047377477@gmail.com>
2024-11-23 21:01:17 +0900

    update : 검색용키워드, 입력중인키워드 분리, tanstack query 무한스크롤

--- src/components/card/CardList.tsx
+++ src/components/card/CardList.tsx
@@ -1,46 +1,27 @@
-import { CardStore, useCardStore } from '@/stores/card/cardStore';
-import { useEffect, useState } from 'react';
 import Loading from '../common/Loading';
 import Card, { CardProps } from './Card';
 import style from './styles/card.module.css';
 
-const CardList = ({ isLoading }: { isLoading: boolean }) => {
-  const baseCardList = useCardStore(
-    (state: CardStore) => state.cardList.baseCardList,
-  );
-  const myCardList = useCardStore(
-    (state: CardStore) => state.cardList.myCardList,
-  );
-  const keywordCardList = useCardStore(
-    (state: CardStore) => state.cardList.keywordCardList,
-  );
-  const tagCardList = useCardStore(
-    (state: CardStore) => state.cardList.tagCardList,
-  );
-
-  const cardListType = useCardStore((state: CardStore) => state.cardListType);
-
-  const [cards, setCards] = useState<CardProps[]>(baseCardList);
-
-  useEffect(() => {
-    if (cardListType === 'list') {
-      setCards(baseCardList);
-    } else if (cardListType === 'my') {
-      setCards(myCardList);
-    } else if (cardListType === 'tag') {
-      setCards(tagCardList);
-    } else if (cardListType === 'keyword') {
-      setCards(keywordCardList);
-    }
-  }, [cardListType, baseCardList, tagCardList, keywordCardList]);
-
+const CardList = ({
+  postList,
+  isLoading,
+  ref,
+  isFetchingNextPage,
+  hasNextPage,
+}: {
+  postList: CardProps[];
+  isLoading: boolean;
+  ref: (node?: Element | null) => void;
+  isFetchingNextPage: boolean;
+  hasNextPage: boolean;
+}) => {
   return (
     <>
       <section className={style.listWrap}>
         <ul className={style.cardListWrap}>
           {!isLoading ? (
-            cards && cards.length > 0 ? (
-              cards.map((card, idx) => {
+            postList && postList.length > 0 ? (
+              postList.map((card, idx) => {
                 return (
                   <li key={idx}>
                     <Card {...card} />
@@ -51,11 +32,14 @@ const CardList = ({ isLoading }: { isLoading: boolean }) => {
               <li className={style.empty}>{'검색된 카드가 없습니다.'}</li>
             )
           ) : (
-            isLoading && (
-              <li key={'loadkey'} className={style.loading}>
-                <Loading color={'#222222'} />
-              </li>
-            )
+            <li key={'loadkey'} className={style.loading}>
+              {isFetchingNextPage && <Loading color={'#222222'} />}
+            </li>
+          )}
+          {hasNextPage && (
+            <li key={'moreFetch'}>
+              <div ref={ref}></div>
+            </li>
           )}
         </ul>
       </section>
