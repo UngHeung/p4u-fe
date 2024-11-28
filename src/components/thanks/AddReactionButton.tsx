6b03395c010118fc78b2e084dceee1bb82171ce1
ungheung <t01047377477@gmail.com>
2024-11-28 16:01:18 +0900

    feat : 리액션 추가, 삭제 / 셀렉터

--- src/components/thanks/AddReactionButton.tsx
+++ src/components/thanks/AddReactionButton.tsx
@@ -1,24 +1,53 @@
 import reactionIcon from '@/public/emoji/emoji.png';
+import { UserProps, useUserStore } from '@/stores/user/userStore';
 import Image from 'next/image';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { MyReactionProps } from './Reaction';
+import ReactionSelector from './ReactionSelector';
 import styles from './styles/thanks.module.css';
 
-const AddReactionButton = () => {
+const AddReactionButton = ({
+  id,
+  reactions,
+  writer,
+  setIsDisabled,
+}: {
+  id: number;
+  reactions: MyReactionProps[];
+  writer: UserProps;
+  setIsDisabled: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const user = useUserStore(state => state.user);
+
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <button
-      className={styles.addReactionButton}
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      <Image
-        src={reactionIcon}
-        alt="reaction"
-        width={75}
-        height={15}
-        sizes="100%"
-      />
-    </button>
+    <>
+      <button
+        className={`${styles.addReactionButton} ${
+          user?.id === writer.id ? styles.myButton : ''
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Image
+          src={reactionIcon}
+          alt="reaction"
+          width={120}
+          height={20}
+          sizes="100%"
+        />
+      </button>
+      {isOpen && (
+        <ReactionSelector
+          id={id}
+          reactions={reactions}
+          writer={writer}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          setIsDisabled={setIsDisabled}
+        />
+      )}
+    </>
   );
 };
 
