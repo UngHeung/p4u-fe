8bc7b82de2510b07d0e95eacd972fbfc8a20a71c
ungheung <t01047377477@gmail.com>
2024-11-30 01:13:14 +0900

    update : 잘못된 접근시 에러페이지

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import AdminMenu from '@/components/admin/AdminMenu';
 import style from '@/components/admin/styles/admin.module.css';
+import ErrorPage from '@/components/common/error/ErrorPage';
+import { useUserStore } from '@/stores/user/userStore';
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const user = useUserStore(state => state.user);
+
+  if (!user || user.role !== 'ROLE_ADMIN') {
+    return <ErrorPage error="403" />;
+  }
+
   return (
     <main className={style.adminMain}>
       <h2 className="a11y-hidden">관리자</h2>
       {children}
-      <AdminMenu />
     </main>
   );
 };
