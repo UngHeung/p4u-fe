802a5263b6ee4ce4f7e602e4b4f9d3561003093a
ungheung <t01047377477@gmail.com>
2024-11-30 00:42:11 +0900

    feat : 비활성화 thanks 목록

--- src/components/admin/inactive/thanks/InactiveThanksList.tsx
+++ src/components/admin/inactive/thanks/InactiveThanksList.tsx
@@ -1,5 +1,37 @@
+'use client';
+
+import { useThanksListQuery } from '@/components/thanks/handlers/useThanksListQuery';
+import styles from '@/components/thanks/styles/thanks.module.css';
+import ThanksBoxList from '@/components/thanks/ThanksBoxList';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
 const InactiveThanksList = () => {
-  return <div>InactiveThanksList</div>;
+  const { data, fetchNextPage, hasNextPage, isLoading } = useThanksListQuery(
+    'inactive',
+    'DESC',
+  );
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  return (
+    <section className={styles.thanksMain}>
+      <section className={styles.listContainer}>
+        <ThanksBoxList
+          data={data}
+          ref={ref}
+          hasNextPage={hasNextPage}
+          isLoading={isLoading}
+        />
+      </section>
+    </section>
+  );
 };
 
 export default InactiveThanksList;
