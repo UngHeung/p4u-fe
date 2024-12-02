a5a2dbcf674979f725bc4d3b9428ee2b540e94a7
ungheung <t01047377477@gmail.com>
2024-12-03 03:30:50 +0900

    feat : alert 인터페이스, 타입, enum 추가

---
+++ src/components/common/alert/const/alertInterface.ts
@@ -0,0 +1,73 @@
+export type StatusTypes = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 500;
+export type AlertTypes = 'SUCCESS' | 'FAILURE' | 'NOTICE';
+export type ReasonTypes =
+  | 'BAD_REQUEST'
+  | 'NOT_FOUND'
+  | 'DUPLICATE'
+  | 'FORBIDDEN'
+  | 'UNAUTHORIZED'
+  | 'ID_OR_PASSWORD'
+  | 'INTERNAL_SERVER_ERROR'
+  | 'NO_CHANGE'
+  | 'NOT_VERIFIED'
+  | 'NOT_FOUND_USER';
+
+export type SolutionTypes =
+  | 'BAD_REQUEST'
+  | 'NOT_FOUND'
+  | 'DUPLICATE'
+  | 'UNAUTHORIZED'
+  | 'ID_OR_PASSWORD'
+  | 'INTERNAL_SERVER_ERROR';
+
+export type TargetTypes =
+  | 'LOGIN'
+  | 'SIGNUP'
+  | 'USER'
+  | 'ID'
+  | 'PASSWORD'
+  | 'ID_OR_PASSWORD'
+  | 'NAME'
+  | 'EMAIL'
+  | 'NICKNAME'
+  | 'RESET_CODE'
+  | 'RESET_PASSWORD'
+  | 'EMAIL_VERIFICATION'
+  | 'EMAIL_VERIFICATION_CODE'
+  | 'NOT_VERIFIED'
+  | 'NICKNAME_ACTIVITY'
+  | 'NICKNAME_DEACTIVITY'
+  | 'NICKNAME_UPDATE'
+  | 'MY_INFO_EDIT'
+  | 'NO_CHANGE'
+  | 'CARD_WRITE'
+  | 'CARD_DELETE'
+  | 'CARD_ACTIVATE'
+  | 'CARD_DEACTIVATE'
+  | 'CARD_REPORT'
+  | 'CARD_REPORT_RESET'
+  | 'CARD_PICK'
+  | 'CARD_UNPICK'
+  | 'CARD_ANSWERED'
+  | 'TAG'
+  | 'THANKS_WRITE'
+  | 'THANKS_EDIT'
+  | 'THANKS_DELETE'
+  | 'THANKS_PICK'
+  | 'THANKS_UNPICK'
+  | 'THANKS_REACTION'
+  | 'THANKS_REACTION_CANCEL';
+
+export interface AlertProps {
+  message: string;
+  index?: number;
+  type: AlertTypes;
+  isExiting?: boolean;
+}
+
+export interface AlertMessageProps {
+  target: TargetTypes;
+  type: AlertTypes;
+  status?: StatusTypes;
+  reason?: ReasonTypes;
+}
